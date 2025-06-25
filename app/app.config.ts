d35d7c483971c7fcfa920c0e7efe4a6e63c75b17
逆行时光 <94590099+nixingshiguang@users.noreply.github.com>
2025-06-25 13:24:11 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   title: 'Sink',
-  email: 'sink.cool@miantiao.me',
+  email: 'admin@160621.xyz',
   github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  twitter: '',
+  telegram: '',
+  mastodon: '',
+  blog: 'https://blog.160621.xyz',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
